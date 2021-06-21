5b50f1d64a8ed6c10d8ab1f98c1cef4bc64c2529
Leszek <lenzuolo@wp.pl>
2021-06-21 10:04:39 +0200

    Header update

--- src/miscellanous/Header/Header.jsx
+++ src/miscellanous/Header/Header.jsx
@@ -1,21 +1,23 @@
-import {HEADERS} from '../../strings';
+import {HEADERS,PATHS} from '../../strings';
+import { useHistory } from 'react-router';
 import './Header.css';
+
 
 const Header = (props) => 
 {
-
+  const history = useHistory();
   let content;
 
   switch(props.type)
   {
     case 'Home':
-        content = <div className='header'>{HEADERS[0]}</div>
+        content = <div className='header' onClick={() => history.push(PATHS.HOMEPAGE)}>{HEADERS[0]}</div>
         break;
     case 'Login':
         content = <div  className='header'> {HEADERS[1]}</div>
         break;
     case 'Panel':
-        content = <div className='header'> {HEADERS[2]}</div>
+        content = <div className='header' onClick={() => history.push(PATHS.EMPLOYEES)}> {HEADERS[2]}</div>
         break;
     default:
         break;
